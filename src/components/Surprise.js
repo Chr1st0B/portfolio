98d2b36536f52a4ac5deeb2e86e36833c61c15ff
Chr1st0B <chr1st0b4r@gmail.com>
2021-06-23 17:03:09 +0200

    portfolioSave3

--- src/components/Surprise.js
+++ src/components/Surprise.js
@@ -1,7 +1,54 @@
+import { useState } from 'react';
+
 const Surprise = () => {
+
+    const [myList, setMyList] = useState([])
+    const [taskName, setTaskName] = useState([])
+
     return(
         <div className="">
-            <h1>HI, I'M CHRISTOPHER</h1>
+            <div className="surpriseBox1">
+                <h1>TO DO LIST</h1>
+                {
+                    myList.map((task, i) => {
+                    console.log(task, i)
+                    return (
+                        <div className='task'>
+                        <span>{task}</span>
+                        <button
+                            onClick={() => {
+                            // faire une copie du tableau
+                            const myListCopy = [...myList]
+                            // delete le ième élément
+                            myListCopy.splice(i, 1)
+                            // setMyList de la copie a laquelle on a retire un element
+                            setMyList(myListCopy)
+                            }}
+                        >X</button>
+                        </div>)
+                    })
+                }
+                <input type='text' value={taskName} onChange={(event) => {
+                    setTaskName(event.target.value)
+                }} />
+                <button
+                    onClick={() => {
+                    // faire une copie du tableau
+                    const myListCopy = [...myList]
+                    // push dans la copie
+                    myListCopy.push(taskName)
+                    // setmyList de la copie
+                    setMyList(myListCopy)
+                    // j'avais pas le droit d'ecrire setMyList(myList.push(taskName))
+                    // car je modifiais myList en direct
+                    // Vider le contenu de l'input en UNE LIGNE
+                    setTaskName('')
+                    }}
+                >AJOUTE MOI</button>
+            </div>
+            <div className="surpriseBox2">
+                
+            </div>
         </div>
     )
 }
